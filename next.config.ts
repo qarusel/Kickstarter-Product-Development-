8851b90e0209d4292cedd9070fe74e5f05a480a3
qarusel <yeskendirmaulet@gmail.com>
2026-03-09 22:06:26 +0500

    Refactor next.config.ts for cleaner syntax

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/Personal-project-attention-span-Yeskendir',
-  assetPrefix: '/Personal-project-attention-span-Yeskendir',
-  images: { unoptimized: true },
-  trailingSlash: true,
+const nextConfig = {
+  output: 'export', 
+  basePath: '/Personal-project-attention-span-Yeskendir', 
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
