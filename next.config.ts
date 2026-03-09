c46c1c28430bf775f280fb599027d8603a3ff083
qarusel <yeskendirmaulet@gmail.com>
2026-03-09 17:44:09 +0500

    Update next.config.ts with new configuration options
    Set output to 'export', define basePath, and configure images.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Обязательно для генерации папки /out
+  basePath: '/Personal-project-attention-span-Yeskendir',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
