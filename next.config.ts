7041c615022f65652156084496496aef46f648fc
qarusel <yeskendirmaulet@gmail.com>
2026-03-09 17:57:48 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'export',
+  // Указываем GitHub Pages, где искать стили и картинки
   basePath: '/Personal-project-attention-span-Yeskendir',
   assetPrefix: '/Personal-project-attention-span-Yeskendir',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
+
+export default nextConfig;
